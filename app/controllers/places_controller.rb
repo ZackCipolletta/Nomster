1e1bbd2c8aaa4394f473784ffad3d982ad3c8fde
ZackCipolletta <ZackCipolletta@gmail.com>
2015-05-03 01:56:18 +0000

    prevented users from destroying place unless creator and signed in

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.page(params[:page]).per(2)
@@ -38,7 +38,7 @@ end
 def destroy
 	@place = Place.find(params[:id])
 	if @place.user != current_user
-		return redirect_to root_path
+		return render :text => 'Not Allowed', :status => :forbidden
 	end
 	@place.destroy
 	redirect_to root_path
