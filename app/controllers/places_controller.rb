5eff58f35819e8f1a402d8ba22cc5a87ae11ca7c
ZackCipolletta <ZackCipolletta@gmail.com>
2015-05-03 02:12:31 +0000

    prevents user from updating a place without all fields entered or creating a place without all fields entered, also highlights error messages when blank fields are submitted.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
+
 	end
 
 def show
@@ -31,8 +36,11 @@ def update
 	if @place.user != current_user
 		return render :text => 'Not Allowed', :status => :forbidden
 	end
-	@place.update_attributes(place_params)
-	redirect_to root_path
+		@place.update_attributes(place_params)
+	if @place.valid?
+		redirect_to root_path
+	else render :edit, :status => :unprocessable_entity
+	end
 end
 
 def destroy
